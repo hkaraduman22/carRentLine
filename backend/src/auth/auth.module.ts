8198eef95a6d82861edd9fdd5f821b6b10ebc06e
Dev221122 <tariklar2322@gmail.com>
2025-12-13 16:52:02 +0300

    a

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
+// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PrismaService } from '../prisma/prisma.service'; // 1. İMPORT ET
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, PrismaService] // 2. BURAYA EKLE
 })
 export class AuthModule {}
