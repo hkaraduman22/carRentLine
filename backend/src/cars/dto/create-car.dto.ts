ca07dafc0d000b6e273098d9fc4841be587a3a12
Dev221122 <tariklar2322@gmail.com>
2025-12-17 21:58:08 +0300

    create.car.dto daki priceparday ekleme problemi cozuldu

--- backend/src/cars/dto/create-car.dto.ts
+++ backend/src/cars/dto/create-car.dto.ts
@@ -1,26 +1,29 @@
 import { Decimal } from "@prisma/client/runtime/library";
+import { Type } from "class-transformer";
 import { IsDecimal, IsNumber,IsString,Min ,Max} from "class-validator";
 
 export class CreateCarDto {
     @IsString()
-    brand:string
+    brand:string;
     @IsString()
-    model:string
+    model:string;
 
     
   @IsNumber({},{message:'Lutfen sayi giriniz'})
   @Min(0,{message:'Lutfen pozitif bir deger giriniz'})
 
+  @Type(()=>Number)//GELEN VERİYİ SAYİYA CEVİR
   km: number;
 
-  @IsDecimal()
+  @IsNumber({},{message:'LUTFEN SAYİ GİRİNİZ'})
   @Min(1000,{message:'lutfen gercekci bir deger giriniz!'})
-   pricePerDay:Decimal
+  @Type(()=>Number)//HATA VEREN KİSİM BU
+   pricePerDay:number
 
 
    @IsNumber()
    @Min(1930,{message:'Lutfen gecerli bir tarih giriniz'})
    year:number
-   
+
 
 }
