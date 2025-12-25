51721fb886f8a1eddaa7d6cf573aea20b49c97b7
Dev221122 <tariklar2322@gmail.com>
2025-12-25 23:29:08 +0300

    aa

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,9 +11,23 @@ async function bootstrap() {
   //TÜR dönüşümü jsonda her şeyi string olarak alır
   app.useGlobalPipes(new ValidationPipe({
     transform:true,
+    //DTO DA TANIMLI DEGİLSE SİL
     whitelist:true,
+    //DTO DA OLMAYAN Requesti kabul etme
+    //Ayrıyeten bunun yazılması gerektiğini bilmiyordum validationpipe direkt kontrol ediyor sanıyordum
     forbidNonWhitelisted:true,
   }))
+  // 2. SWAGGER AYARLARI (BURAYI EKLE)
+  const config = new DocumentBuilder()
+    .setTitle('Araç Kiralama API') // Başlık
+    .setDescription('Rent A Car projesi için API dokümantasyonu') // Açıklama
+    .setVersion('1.0') // Versiyon
+    .addBearerAuth() // Token kilit simgesini ekle (Auth işlemleri için)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // 'localhost:3000/api' adresinde çalışsın
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
