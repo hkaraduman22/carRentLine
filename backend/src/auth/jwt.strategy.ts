73003e3d25d65285c717657a6ad5c1dfa6afa420
Dev221122 <tariklar2322@gmail.com>
2026-01-15 23:57:58 +0300

    last version of project

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,6 +1,5 @@
  
-
-import { ExtractJwt, Strategy } from 'passport-jwt';
+ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 
